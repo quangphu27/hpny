f45faeb86580b3df1cf2f8ccc477321e11697170
tranvanquangphu <phu2722002@gmail.com>
2025-01-28 11:52:58 +0700

    Initial commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const data_name = [
+    {
+        ma_ten: "phu    ",
+        name: "Quang PhúPhú",
+        message: "Hello, I'm Phú Phá PháchPhách",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    },
+    {
+        ma_ten: "nhung",
+        name: "Mỹ Nhung",
+        message: "Hello, I'm Nhung Nhí Nhảnh",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    }
+]
